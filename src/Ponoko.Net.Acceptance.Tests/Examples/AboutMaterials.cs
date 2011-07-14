22d24b08fb86fdc85c6c4bbb43a0c1696409c269
Ben Biddington <ben.biddington@gmail.com>
2011-07-14 12:13:57 +1200

    Before finding materials you need a node key

--- src/Ponoko.Net.Acceptance.Tests/Examples/AboutMaterials.cs
+++ src/Ponoko.Net.Acceptance.Tests/Examples/AboutMaterials.cs
@@ -6,20 +6,34 @@ using Ponoko.Api.Json;
 namespace Ponoko.Api.Acceptance.Tests.Examples {
 	[TestFixture]
 	public class AboutMaterials : AcceptanceTest {
+		private String _firstNodeKey;
+
+		[Test]
+		public void first_you_need_key_for_a_making_node() {
+			var uri = Map("/nodes");
+			
+			using (var response = Get(uri, Credentials)) {
+				var json = new Deserializer().Deserialize(Body(response));
+
+				var theNodeKey = json["nodes"].First.Value<String>("key");
+				
+				Assert.IsNotEmpty(theNodeKey, "Expected at least one node key.");
+			}
+		}
+
 		[Test]
 		public void can_get_materials_catalogue() {
-			var nodeKey = "2e9d8c90326e012e359f404062cdb04a";
+			var nodeKey = FirstNodeKey;
 			var uri = Map("{0}/{1}", "/nodes/material-catalog", nodeKey);
 
 			using (var response = Get(uri, Credentials)) {
 				Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "Expected okay");
-				Console.WriteLine(Json(response));
 			}
 		}
 
 		[Test]
 		public void and_you_can_deserialize_the_result() {
-			var nodeKey = "2e9d8c90326e012e359f404062cdb04a";
+			var nodeKey = FirstNodeKey;
 			var uri = Map("{0}/{1}", "/nodes/material-catalog", nodeKey);
 
 			using (var response = Get(uri, Credentials)) {
@@ -27,6 +41,20 @@ namespace Ponoko.Api.Acceptance.Tests.Examples {
 				var json = Json(response);
 				var result = MaterialListDeserializer.Deserialize(json);
 				Assert.AreEqual(347, result.Length);
+			}
+		}
+
+		private String FirstNodeKey {
+			get { return _firstNodeKey ?? (_firstNodeKey = FindFirstNodeKey()); }
+		}
+
+		private string FindFirstNodeKey() {
+			var uri = Map("/nodes");
+
+			using (var response = Get(uri, Credentials)) {
+				var json = new Deserializer().Deserialize(Body(response));
+
+				return json["nodes"].First.Value<String>("key");
 			}
 		}
 	}
