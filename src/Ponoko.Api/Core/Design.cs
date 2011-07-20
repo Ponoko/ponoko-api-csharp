e2126572a7ea9f1d61e0c0b7868037d3af6faa5b
Ben Biddington <ben.biddington@gmail.com>
2011-07-21 11:57:14 +1200

    Refactor: classes to their own files

---
+++ src/Ponoko.Api/Core/Design.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Ponoko.Api.Core {
+	public class Design {
+		public Int64 Size{ get; set; }
+
+		[JsonProperty(PropertyName = "created_at")]
+		public DateTime CreatedAt { get; set; }
+
+		public Int32 Quantity { get; set; }
+
+		[JsonProperty(PropertyName = "content_type")]
+		public String ContentType { get; set; }
+
+		[JsonProperty(PropertyName = "updated_at")]
+		public DateTime UpdatedAt { get; set; }
+
+		[JsonProperty(PropertyName = "material_key")]
+		public String MaterialKey { get; set; }
+
+		public String Filename { get; set; }
+
+		[JsonProperty(PropertyName = "ref")]
+		public String Reference { get; set; }
+
+		public String Key { get; set; }
+
+		[JsonProperty(PropertyName = "make_cost")]
+		public MakeCost MakeCost { get; set; }
+	}
+}
---
+++ src/Ponoko.Api/Core/MakeCost.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Ponoko.Api.Core {
+	public class MakeCost {
+		public Decimal Total { get; set; }
+		public Decimal Making { get; set; }
+		public Decimal Materials { get; set; }
+		public String Currency { get; set; }
+	}
+}
