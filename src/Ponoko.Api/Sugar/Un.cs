4d6186cad2b47d1b67bec2fa5ad51cfdeb0113e4
Ben Biddington <ben.biddington@gmail.com>
2011-07-22 11:32:50 +1200

    Added overload so the condition can be a Boolean

--- src/Ponoko.Api/Sugar/Un.cs
+++ src/Ponoko.Api/Sugar/Un.cs
@@ -2,6 +2,10 @@
 
 namespace Ponoko.Api.Sugar {
 	public static class un {
+		public static void less(Boolean condition, Action thenWhat) {
+			un.less(() => condition, thenWhat);
+		}
+
 		public static void less(Func<bool> condition, Action thenWhat) {
 			if (condition() == false) {
 				thenWhat();
