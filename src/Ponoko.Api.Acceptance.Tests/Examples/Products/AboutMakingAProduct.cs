1a1d7f71727a1e061cf36d23fe8bb7baf4476a65
Ben Biddington <ben.biddington@gmail.com>
2011-07-27 12:33:52 +1200

    you_can_get_the_shipping_options_for_multiple_products

--- src/Ponoko.Api.Acceptance.Tests/Examples/Products/AboutMakingAProduct.cs
+++ src/Ponoko.Api.Acceptance.Tests/Examples/Products/AboutMakingAProduct.cs
@@ -1,6 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Ponoko.Api.Core.Product;
 using Ponoko.Api.Core.Product.Commands;
+using Ponoko.Api.Core.Shipping;
 using FindCommand = Ponoko.Api.Core.Shipping.Commands.FindCommand;
 
 namespace Ponoko.Api.Acceptance.Tests.Examples.Products {
@@ -10,12 +12,16 @@ namespace Ponoko.Api.Acceptance.Tests.Examples.Products {
 
 		[TestFixtureTearDown]
 		public void TestFixtureTearDown() {
-			new DeleteCommand(Internet, Settings.BaseUrl).Delete(ExampleProduct.Key);
+			Assert.Throws<Exception>(() => 
+				new DeleteCommand(Internet, Settings.BaseUrl).Delete(ExampleProduct.Key), 
+                "Expected this to fail because the delete operation is broken on the server end. " + 
+                "It didn't fail, so the remote end has been fixed and you can remove this assertion."
+			);
 		}
 
 		[Test]
 		public void you_can_get_the_shipping_options_for_a_single_product() {
-			var result = new FindCommand(Internet, Settings.BaseUrl).For(ExampleProduct);
+			var result = new FindCommand(Internet, Settings.BaseUrl).For(ExampleShippingInfo);
 
 			Assert.AreEqual("USD", result.Currency, "Unexpected currency code returned.");
 			Assert.AreEqual(ExampleProduct.Key, result.Products[0].Key, "Unexpected key returned.");
@@ -24,8 +30,25 @@ namespace Ponoko.Api.Acceptance.Tests.Examples.Products {
 			Assert.That(result.Options[0].Price, Is.GreaterThan(0.00d), "Unexpected price for the first option, expected non-zero value.");
 		}
 
+		[Test]
+		public void you_can_get_the_shipping_options_for_multiple_products() {
+			var multipleProducts = new [] {ExampleShippingInfo, ExampleShippingInfo};
+			var result = new FindCommand(Internet, Settings.BaseUrl).For(multipleProducts);
+
+			Assert.AreEqual(2, result.Products.Count, "Expected number of products returned");
+		}
+
 		private Product ExampleProduct {
 			get { return _exampleProduct ?? (_exampleProduct = GetFirstProduct()); }
+		}
+
+		public ProductShippingInfo ExampleShippingInfo { 
+			get {
+				return new ProductShippingInfo {
+					Key = ExampleProduct.Key,
+					Quantity = 1
+				};
+			}
 		}
 
 		private Product GetFirstProduct() {
@@ -37,5 +60,6 @@ namespace Ponoko.Api.Acceptance.Tests.Examples.Products {
 		}
 
 		// TEST: you_have_to_supply_a_product
+		// TEST: you_can_order_more_that_one
 	}
 }
--- src/Ponoko.Api/Core/Shipping/Commands/FindCommand.cs
+++ src/Ponoko.Api/Core/Shipping/Commands/FindCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Ponoko.Api.Json;
 using Ponoko.Api.Rest;
 
@@ -6,18 +7,26 @@ namespace Ponoko.Api.Core.Shipping.Commands {
 	public class FindCommand : Domain {
 		public FindCommand(TheInternet internet, string baseUrl) : base(internet, baseUrl) {}
 
-		public ShippingOptions For(Product.Product product) {
+		public ShippingOptions For(params ProductShippingInfo[] products) {
+			var productsString = new StringBuilder();
+
+			foreach (var info in products) {
+				productsString.AppendFormat(
+					"products[][key]={0}&products[][quantity]={1}&", 
+					info.Key, info.Quantity
+				);
+			}
+
 			Uri uri = Map(
 				"/orders/shipping-options?" + 
-				"products[][key]={0}&" + 
-				"products[][quantity]=1&" + 
+				"{0}" + 
 				"delivery_address[address_line_1]=27%20Dixon%20Street&" + 
 				"delivery_address[address_line_2]=Te%20Aro&"+ 
 				"delivery_address[city]=Wellington&" + 
 				"delivery_address[state]=na&" + 
 				"delivery_address[zip_or_postal_code]=6021&" +
 				"delivery_address[country]=New%20Zealand", 
-				product.Key
+				productsString
 			);
 
 			var response = _internet.Get(uri);
