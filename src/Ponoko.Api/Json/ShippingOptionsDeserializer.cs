03a2d116fde8c8a058f045ad93fae8af252e9993
Ben Biddington <ben.biddington@gmail.com>
2011-07-28 12:48:19 +1200

    Removed debug output

--- src/Ponoko.Api/Json/ShippingOptionsDeserializer.cs
+++ src/Ponoko.Api/Json/ShippingOptionsDeserializer.cs
@@ -11,8 +11,6 @@ namespace Ponoko.Api.Json {
 
 			var payload = new Deserializer().Deserialize(json);
 			
-			Console.WriteLine(json);
-			
 			return JsonConvert.DeserializeObject<ShippingOptions>(payload["shipping_options"].ToString(), settings);
 		}
 	}
