0cd3344206f3eda13d346489e7a0c9ff53a4794c
Ben Biddington <ben.biddington@gmail.com>
2011-07-28 12:47:44 +1200

    Modified so it only adds parameters with non-empty values. Not sure if this behaviour belongs here.

--- src/Ponoko.Api/Rest/Mime/FormUrlEncoded.cs
+++ src/Ponoko.Api/Rest/Mime/FormUrlEncoded.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Text;
 using System.IO;
-using Ponoko.Api.Core;
 
 namespace Ponoko.Api.Rest.Mime {
 	// See: http://www.w3.org/TR/REC-html40/interact/forms.html#h-17.13.4.1
@@ -28,19 +26,19 @@ namespace Ponoko.Api.Rest.Mime {
 			var buffer = new StringBuilder();
 			
 			foreach (var parameter in parameters) {
-				buffer.AppendFormat("{0}={1}&", UrlEncode(parameter.Name), UrlEncode(parameter.Value));
+				var theParameterHasAValue = parameter.Value != null;
+				// TODO: I think the server end handles empty parameters differently. If you supply an empty parameter value you may get
+				// "Invalid OAuth Request" error. Consider expressing this elsewhere.
+				if (theParameterHasAValue) {
+					buffer.AppendFormat("{0}={1}&", UrlEncode(parameter.Name), UrlEncode(parameter.Value));
+				}
 			}
 
 			return buffer.ToString().TrimEnd('&');
-
-			//return String.Join("&", 
-			//    Array.ConvertAll(parameters, 
-			//        key => String.Format("{0}={1}", UrlEncode(key), UrlEncode(parameters[key]))
-			//    )
-			//);
 		}
 
 		private String UrlEncode(Object what) {
+			if (null == what) return null;
 			return Uri.EscapeDataString(what.ToString());
 		}
 
