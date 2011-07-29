d29b890bb205aee4839d21b02c824d51eaaa12f7
Ben Biddington <ben.biddington@gmail.com>
2011-07-29 15:14:34 +1200

    (FAILING) you_can_get_the_list_of_orders

---
+++ src/Ponoko.Api.Acceptance.Tests/Examples/Products/AboutOrderHistory.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Ponoko.Api.Core.Orders;
+
+namespace Ponoko.Api.Acceptance.Tests.Examples.Products {
+	[TestFixture]
+	public class AboutOrderHistory : ProductAcceptanceTest {
+		[Test]
+		public void you_can_get_the_list_of_orders() {
+			var finder = new FindCommand(Internet, Settings.BaseUrl);
+			
+			var result = finder.All();
+
+			Assert.Fail(
+				"Even though the query was successful, " + 
+				"this can't be tested until we're able to create orders."
+			);
+		}
+	}
+}
---
+++ src/Ponoko.Api/Core/Orders/FindCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ponoko.Api.Core.Shipping;
+using Ponoko.Api.Rest;
+
+namespace Ponoko.Api.Core.Orders {
+	public class FindCommand : Domain {
+		public FindCommand(TheInternet internet, String baseUrl) : base(internet, baseUrl) {}
+
+		public Order[] All() {
+			var uri = Map("/orders");
+
+			using (var response = Get(uri)) {
+				Console.WriteLine(ReadAll(response));
+			}
+
+			return new Order[0];
+		}
+	}
+}
