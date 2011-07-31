8d9d44e9b68738f68721de965e4b2dd3757ce786
Ben Biddington <ben.biddington@gmail.com>
2011-08-01 11:47:52 +1200

    FileSystem it_opens_the_file_supplied and it_does_not_create_the_file_if_it_does_not_exist

--- src/Ponoko.Api.Integration.Tests/Core/IO/FileSystemTests.cs
+++ src/Ponoko.Api.Integration.Tests/Core/IO/FileSystemTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using NUnit.Framework;
 using Ponoko.Api.Core.IO;
 
@@ -11,7 +8,7 @@ namespace Ponoko.Api.Integration.Tests.Core.IO {
 	public class FileSystemTests {
 		[Test]
 		public void it_creates_a_file_on_disk() {
-			var theFilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+			var theFilePath = NewRandomTempFile();
 			
 			Assert.IsFalse(File.Exists(theFilePath), "Invalid test data, the file must not exist before it is created!");
 			
@@ -20,17 +17,71 @@ namespace Ponoko.Api.Integration.Tests.Core.IO {
 			Assert.IsTrue(File.Exists(theFilePath), "Expected the file to have been created on disk");
 		}
 
-		// TEST: new_files_are_writable_and_readable
+		[Test]
+		public void it_can_open_any_file_for_read_and_write_and_seek() {
+			var fileSystem = new DefaultFileSystem();
+
+			var anyFile = fileSystem.New(NewRandomTempFile());
+			
+			using (var theStream = fileSystem.Open(anyFile)) {
+				Assert.IsTrue(theStream.CanRead	, "Expected the resultant stream to be readable");
+				Assert.IsTrue(theStream.CanWrite, "Expected the resultant stream to be writable");
+				Assert.IsTrue(theStream.CanSeek	, "Expected the resultant stream to be seekable");
+			}
+		}
+
+		[Test]
+		public void it_opens_the_file_supplied() {
+			var fileSystem = new DefaultFileSystem();
+
+			var anyFile = fileSystem.New(NewRandomTempFile());
+
+			var theExpectedText = "Let’s make toast the American way, you burn it and I’ll scrape it.";
+
+			using (var theStream = fileSystem.Open(anyFile)) {
+				using (var writer = new StreamWriter(theStream)) {
+					writer.Write(theExpectedText);
+				}
+			}
+
+			var theActualTextInTheFile = File.ReadAllText(anyFile.FullName);
+
+			Assert.AreEqual(theExpectedText, theActualTextInTheFile, 
+				"Expected the text in the file <{0}> to match because we asked DefaultFileSystem to " + 
+				"open it and then we wrote that text to it. " + 
+				"This means the file we asked to be opened was not opened (because the write was successful).", 
+				anyFile
+			);
+		}
+
+		[Test] 
+		public void it_does_not_create_the_file_if_it_does_not_exist() {
+			var fileSystem = new DefaultFileSystem();
+
+			var aFileThatDoesNotExist = new FileInfo("xxx.xxx");
+			
+			File.Delete(aFileThatDoesNotExist.FullName);
+
+			Assert.IsFalse(aFileThatDoesNotExist.Exists);
+
+			var theError = Assert.Throws<FileNotFoundException>(() => fileSystem.Open(aFileThatDoesNotExist));
+
+			Assert.That(theError.Message, Is.StringMatching("^Could not find file"));
+		}
+
+		private String NewRandomTempFile() {
+			return Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+		}
 	}
 
 	public class DefaultFileSystem : FileSystem {
 		public FileInfo New(string filename) {
-			using(var newFile = File.Create(filename)) {}
+			using (var newFile = File.Create(filename)) {}
 			return new FileInfo(filename);
 		}
 
 		public FileStream Open(FileInfo filename) {
-			throw new NotImplementedException();
+			return File.Open(filename.FullName, FileMode.Open);
 		}
 
 		public void Delete(FileInfo filename) {
