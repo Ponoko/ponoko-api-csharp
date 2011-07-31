032da20bb080f5d29f5736371422792d7d551eab
Ben Biddington <ben.biddington@gmail.com>
2011-08-01 11:13:24 +1200

    FileSystem it_creates_a_file_on_disk

---
+++ src/Ponoko.Api.Integration.Tests/Core/IO/FileSystemTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Ponoko.Api.Core.IO;
+
+namespace Ponoko.Api.Integration.Tests.Core.IO {
+	[TestFixture]
+	public class FileSystemTests {
+		[Test]
+		public void it_creates_a_file_on_disk() {
+			var theFilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+			
+			Assert.IsFalse(File.Exists(theFilePath), "Invalid test data, the file must not exist before it is created!");
+			
+			new DefaultFileSystem().New(theFilePath);
+
+			Assert.IsTrue(File.Exists(theFilePath), "Expected the file to have been created on disk");
+		}
+
+		// TEST: new_files_are_writable_and_readable
+	}
+
+	public class DefaultFileSystem : FileSystem {
+		public FileInfo New(string filename) {
+			using(var newFile = File.Create(filename)) {}
+			return new FileInfo(filename);
+		}
+
+		public FileStream Open(FileInfo filename) {
+			throw new NotImplementedException();
+		}
+
+		public void Delete(FileInfo filename) {
+			throw new NotImplementedException();
+		}
+	}
+}
