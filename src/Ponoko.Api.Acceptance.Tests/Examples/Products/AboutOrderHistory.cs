905c2c8d07a761e41ca8c7d5d5d918b2bdbd1d70
Ben Biddington <ben.biddington@gmail.com>
2011-08-11 11:11:32 +1200

    you_can_get_the_list_of_orders but you_cannot_delete_an_order

--- src/Ponoko.Api.Acceptance.Tests/Examples/Products/AboutOrderHistory.cs
+++ src/Ponoko.Api.Acceptance.Tests/Examples/Products/AboutOrderHistory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using Ponoko.Api.Core.Orders;
 using Ponoko.Api.Core.Orders.Commands;
+using Ponoko.Api.Core.Product.Commands;
 using Ponoko.Api.Core.Shipping;
 using Ponoko.Api.Core.Shipping.Commands;
 using System.Linq;
@@ -10,13 +11,29 @@ using System.Linq;
 namespace Ponoko.Api.Acceptance.Tests.Examples.Products {
 	[TestFixture]
 	public class AboutOrderHistory : OrderingAcceptanceTest {
+		public Order NewOrder { get; set; }
+		
+		[SetUp]
+		public void TestFixtureSetUp() {
+			NewOrder = CreateANewOrder();
+		}
+
 		[Test]
 		public void you_can_get_the_list_of_orders() {
-			var aNewOrder = CreateANewOrder();
-
 			var result = new OrderHistory(Internet, Settings.BaseUrl).FindAll();
 
-			AssertTheOrderIsReturned(result, aNewOrder.Reference);
+			AssertTheOrderIsReturned(result, NewOrder.Reference);
+		}
+
+		[Test]
+		public void you_cannot_delete_an_order() {
+			var uri = Map("/orders/delete/{0}", NewOrder.Key);
+
+			var theError = Assert.Throws<Exception>(() 
+				=> new DeleteResourceCommand(Internet, Settings.BaseUrl).Delete(uri)
+			);
+
+			Assert.That(theError.Message, Is.StringMatching("^Delete failed. The server returned status NotFound \\(404\\).+"));
 		}
 
 		private void AssertTheOrderIsReturned(IEnumerable<Order> orders, String reference) {
