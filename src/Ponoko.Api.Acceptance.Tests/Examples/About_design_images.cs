299a0fe25c11013de05156cede093af7286721fa
Ben Biddington <ben.biddington@gmail.com>
2011-09-23 16:34:43 +1200

    Extracted DesignImageAddCommand to its own file

--- src/Ponoko.Api.Acceptance.Tests/Examples/About_design_images.cs
+++ src/Ponoko.Api.Acceptance.Tests/Examples/About_design_images.cs
@@ -4,10 +4,7 @@ using System.Security.Cryptography;
 using System.Text;
 using NUnit.Framework;
 using Ponoko.Api.Acceptance.Tests.Examples.Products;
-using Ponoko.Api.Core;
-using Ponoko.Api.Core.Product;
-using Ponoko.Api.Json;
-using Ponoko.Api.Rest;
+using Ponoko.Api.Core.Product.Commands;
 
 namespace Ponoko.Api.Acceptance.Tests.Examples {
 	[TestFixture]
@@ -91,58 +88,5 @@ namespace Ponoko.Api.Acceptance.Tests.Examples {
 
 		[Test, Ignore("PENDING")]
 		public void you_may_get_an_auto_generated_image() { }
-	}
-
-	public class DesignImageAddCommand : Domain {
-		public DesignImageAddCommand(TheInternet internet, String baseUrl) : base(internet, baseUrl) { }
-
-		public Product Add(String product, FileInfo file) {
-			var uri = Map("/products/{0}/design-images", product);
-
-			var payload = new Payload { { "design_images[][uploaded_data]", new DataItem(file, "image/gif") } };
-
-			using (var response = MultipartPost(uri, payload)) {
-				return Deserialize(response);
-			}
-		}
-
-		public Byte[] Get(String product, String filename) {
-			var uri = Map("/products/{0}/design-images/download?filename={1}", product, filename);
-
-			using (var response = Get(uri)) {
-				var length = Int32.Parse(response.Header("Content-length"));
-				return ReadAll(response.Open(), length);
-			}
-		}
-
-		public Product Remove(String product, String filename) {
-			var uri = Map("/products/{0}/design-images/destroy?filename={1}", product, filename);
-
-			using (var response = Get(uri)) {
-				return Deserialize(response);
-			}
-		}
-
-		private Product Deserialize(Response response) {
-			var json = ReadAll(response);
-
-			var productJson = new Deserializer().Deserialize(json)["product"];
-			return ProductDeserializer.Deserialize(productJson.ToString());
-		}
-
-		private byte[] ReadAll(Stream input, Int32 length) {
-			const Int32 BUFFER_SIZE = 1024 * 10;
-
-			using (var output = new MemoryStream(length)) {
-				var buffer = new Byte[BUFFER_SIZE];
-				var bytesRead = 0;
-
-				while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0) {
-					output.Write(buffer, 0, bytesRead);
-				}
-
-				return output.GetBuffer();
-			}
-		}
 	}
 }
---
+++ src/Ponoko.Api/Core/Product/Commands/DesignImageAddCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Ponoko.Api.Json;
+using Ponoko.Api.Rest;
+
+namespace Ponoko.Api.Core.Product.Commands {
+	public class DesignImageAddCommand : Domain {
+		public DesignImageAddCommand(TheInternet internet, String baseUrl) : base(internet, baseUrl) { }
+
+		public Product Add(String product, FileInfo file) {
+			var uri = Map("/products/{0}/design-images", product);
+
+			var payload = new Payload { { "design_images[][uploaded_data]", new DataItem(file, "image/gif") } };
+
+			using (var response = MultipartPost(uri, payload)) {
+				return Deserialize(response);
+			}
+		}
+
+		public Byte[] Get(String product, String filename) {
+			var uri = Map("/products/{0}/design-images/download?filename={1}", product, filename);
+
+			using (var response = Get(uri)) {
+				var length = Int32.Parse(response.Header("Content-length"));
+				return ReadAll(response.Open(), length);
+			}
+		}
+
+		public Product Remove(String product, String filename) {
+			var uri = Map("/products/{0}/design-images/destroy?filename={1}", product, filename);
+
+			using (var response = Get(uri)) {
+				return Deserialize(response);
+			}
+		}
+
+		private Product Deserialize(Response response) {
+			var json = ReadAll(response);
+
+			var productJson = new Deserializer().Deserialize(json)["product"];
+			return ProductDeserializer.Deserialize(productJson.ToString());
+		}
+
+		private byte[] ReadAll(Stream input, Int32 length) {
+			const Int32 BUFFER_SIZE = 1024 * 10;
+
+			using (var output = new MemoryStream(length)) {
+				var buffer = new Byte[BUFFER_SIZE];
+				var bytesRead = 0;
+
+				while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0) {
+					output.Write(buffer, 0, bytesRead);
+				}
+
+				return output.GetBuffer();
+			}
+		}
+	}
+}
