ae27ffd301e6244815ae0fc897b08fbb5aed7c3b
Ben Biddington <ben.biddington@gmail.com>
2011-09-29 12:21:14 +1300

    Describing that DesignImage may represent either a file on disk or a file name (not sure if this's senssible yet)

---
+++ src/Ponoko.Api.Unit.Tests/Core/Product/DesignImageTests.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using NUnit.Framework;
+using Ponoko.Api.Core.Product;
+
+namespace Ponoko.Api.Unit.Tests.Core.Product {
+	[TestFixture]
+	public class DesignImageTests {
+		[Test]
+		public void full_name_equals_filename_when_there_is_no_file_info() {
+			var designImage = new DesignImage {Filename = "xxx"};
+			Assert.AreEqual(designImage.Filename, designImage.FullName);
+		}
+
+		[Test]
+		public void full_name_is_the_same_as_the_fileinfo_supplied_to_ctor() {
+			var fileInfo = new FileInfo("D:\\path\\to\\xxx");
+			var designImage = new DesignImage(fileInfo);
+			Assert.AreEqual(fileInfo.FullName, designImage.FullName);
+		}
+
+		[Test]
+		public void when_file_info_supplied_to_ctor_file_name_is_taken_from_that() {
+			var fileInfo = new FileInfo("D:\\path\\to\\xxx");
+			var designImage = new DesignImage(fileInfo);
+			Assert.AreEqual(fileInfo.Name, designImage.Filename);
+		}
+
+		[Test]
+		public void setting_file_name_clears_the_path_from_full_name_too() {
+			var fileInfo = new FileInfo("D:\\path\\to\\xxx");
+			var designImage = new DesignImage(fileInfo);
+
+			designImage.Filename = "yyy_my_delilah";
+			
+			Assert.AreEqual(designImage.Filename, designImage.FullName);
+		}
+	}
+}
--- src/Ponoko.Api/Core/Product/DesignImage.cs
+++ src/Ponoko.Api/Core/Product/DesignImage.cs
@@ -1,7 +1,27 @@
 ﻿using System;
+using System.IO;
 
 namespace Ponoko.Api.Core.Product {
 	public class DesignImage {
-		public String Filename { get; set; }
+		private String _fullName;
+		private String _fileName;
+
+		public String Filename {
+			get { return _fileName; }
+			set { _fileName = _fullName = value; }
+		}
+
+		public DesignImage() : this(null) {}
+
+		public DesignImage(FileInfo fileInfo) {
+			if (fileInfo != null) {
+				_fullName = fileInfo.FullName;
+				_fileName = fileInfo.Name;
+			}
+		}
+
+		public String FullName {
+			get { return _fullName ?? Filename; }
+		}
 	}
 }
