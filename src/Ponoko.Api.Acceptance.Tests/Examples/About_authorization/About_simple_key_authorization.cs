1925b8cb8143ce714e4b18b9024b730972463612
Ben Biddington <ben.biddington@gmail.com>
2011-11-14 12:09:15 +1300

    About_simple_key_authorization if_you_supply_invalid_credentials_you_get_an_error

--- src/Ponoko.Api.Acceptance.Tests/Examples/About_authorization/About_simple_key_authorization.cs
+++ src/Ponoko.Api.Acceptance.Tests/Examples/About_authorization/About_simple_key_authorization.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Ponoko.Api.Core.Repositories;
 using Ponoko.Api.Rest;
 using Ponoko.Api.Rest.Logging;
@@ -17,5 +18,19 @@ namespace Ponoko.Api.Acceptance.Tests.Examples.About_authorization {
 			Assert.That(nodes.FindAll().Count, Is.GreaterThan(0), "Expected at least one making node to be returned.");
     	}
 
+		[Test]
+        public void if_you_supply_invalid_credentials_you_get_an_error() {
+			var authPolicy = new SimpleKeyAuthorizationPolicy(
+				new SimpleKeyAuthorizationCredential("xxx_clearly_invalid", "")
+			);
+				
+			var theInternetWithSimpleKeyAuthorization =  new SystemInternet(authPolicy, new ConsoleLog());
+
+			var nodes = new Nodes(theInternetWithSimpleKeyAuthorization, Settings.BaseUrl);
+
+			var theError = Assert.Throws<Exception>(() => nodes.FindAll());
+
+			Assert.That(theError.Message, Is.StringEnding("The server returned status Unauthorized (401), and error message: \"Unauthorized\""));
+		}
 	}
 }
