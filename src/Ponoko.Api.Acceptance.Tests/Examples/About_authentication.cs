a5fe364b2d69b8005b1849610ba787fc2535de76
Ben Biddington <ben.biddington@gmail.com>
2011-11-14 11:06:48 +1300

    You can use oauth or simple key authorization

---
+++ src/Ponoko.Api.Acceptance.Tests/Examples/About_authentication.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using NUnit.Framework;
+using Ponoko.Api.Core.Repositories;
+using Ponoko.Api.Rest;
+using Ponoko.Api.Rest.Security;
+using Ponoko.Api.Rest.Security.OAuth.Core;
+using Ponoko.Api.Rest.Security.OAuth.Http;
+using Ponoko.Api.Rest.Security.OAuth.Impl.OAuth.Net;
+
+namespace Ponoko.Api.Acceptance.Tests.Examples {
+	public class About_authentication : AcceptanceTest {
+    	[Test]
+        public void you_can_use_oauth() {
+			var authPolicy = new OAuthAuthorizationPolicy(
+				new MadgexOAuthHeader(
+					new SystemClock(),
+					new SystemNonceFactory()
+				),
+				Settings.Credentials
+			);
+				
+			var theInternetWithOAuth =  new SystemInternet(authPolicy);
+
+			var nodes = new Nodes(theInternetWithOAuth, Settings.BaseUrl);
+
+    		Assert.That(nodes.FindAll().Count, Is.GreaterThan(0), "Expected at least one making node to be returned.");
+    	}
+
+		[Test]
+        public void you_can_use_simple_key_authorization() {
+			var authPolicy = new SimpleKeyAuthorization(Settings.SimpleKeyAuthorizationCredential);
+				
+			var theInternetWithsimpleKeyauthorization =  new SystemInternet(authPolicy);
+
+			var nodes = new Nodes(theInternetWithsimpleKeyauthorization, Settings.BaseUrl);
+
+			Assert.That(nodes.FindAll().Count, Is.GreaterThan(0), "Expected at least one making node to be returned.");
+    	}
+	}
+
+	internal class SimpleKeyAuthorizationTests {
+		[Test]
+		public void it_adds_the_key_to_the_url() {
+			var simpleKeyAuthorizer = new SimpleKeyAuthorization(
+				new SimpleKeyAuthorizationCredential("abcdefgh", "stuvwxyz")
+			);
+
+			var request = new Request(RequestLine.Get(new Uri("http://xxx/")));
+			Request authorized = simpleKeyAuthorizer.Authorize(request);
+
+			var expected = new Uri("http://xxx/?app_key=abcdefgh&user_access_key=stuvwxyz");
+			Assert.AreEqual(expected, authorized.RequestLine.Uri);
+		}
+
+		// [Test] it_preserves_other_params
+		// [Test] it_fails_if_url_already_contains_either_parameter
+	}
+
+	public class SimpleKeyAuthorizationCredential {
+		public readonly string AppKey;
+		public readonly string UserAccessKey;
+
+		public SimpleKeyAuthorizationCredential(String appKey, String userAccessKey) {
+			AppKey = appKey;
+			UserAccessKey = userAccessKey;
+		}
+	}
+
+	public class SimpleKeyAuthorization : AuthorizationPolicy {
+		private readonly SimpleKeyAuthorizationCredential _credential;
+
+		public SimpleKeyAuthorization(SimpleKeyAuthorizationCredential credential) {
+			_credential = credential;
+		}
+
+		public Request Authorize(Request request) {
+			var newUri = new Uri(
+				request.RequestLine.Uri + 
+				"?app_key=" + _credential.AppKey + 
+				"&user_access_key=" + _credential.UserAccessKey
+			);
+			var newRequestLine = new RequestLine(request.RequestLine.Verb, newUri, request.RequestLine.Version);
+			return new Request(newRequestLine, request.Headers, request.Payload);
+		}
+	}
+}
--- src/Ponoko.Api.Acceptance.Tests/Settings.cs
+++ src/Ponoko.Api.Acceptance.Tests/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using Ponoko.Api.Acceptance.Tests.Examples;
 using Ponoko.Api.Rest.Security.OAuth.Core;
 
 namespace Ponoko.Api.Acceptance.Tests {
@@ -10,6 +11,15 @@ namespace Ponoko.Api.Acceptance.Tests {
 
 		public static String BaseUrl { 
 			get { return ConfigurationManager.AppSettings["Ponoko.Api.BaseUrl"]; }
+		}
+
+		public static SimpleKeyAuthorizationCredential SimpleKeyAuthorizationCredential {
+			get { 
+				return new SimpleKeyAuthorizationCredential(
+					ConfigurationManager.AppSettings["Ponoko.SimpleKeyAuthorization.AppKey"], 
+					ConfigurationManager.AppSettings["Ponoko.SimpleKeyAuthorization.UserAccessKey"]
+				); 
+			} 
 		}
 
 		private static readonly Credential _consumer = new Credential(
