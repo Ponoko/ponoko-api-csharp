c910fb157d6cc01fe0e4d3af7f02c22e624c18eb
Ben Biddington <ben.biddington@gmail.com>
2011-11-14 11:43:33 +1300

    Refactor: moved classes to the correct packages. Also: SimpleKeyAuthorization#Authorize it_preserves_the_path

---
+++ src/Ponoko.Api.Rest/Security/Simple/SimpleKeyAuthorizationCredential.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ponoko.Api.Rest.Security.Simple {
+	public class SimpleKeyAuthorizationCredential {
+		public readonly string AppKey;
+		public readonly string UserAccessKey;
+
+		public SimpleKeyAuthorizationCredential(String appKey, String userAccessKey) {
+			AppKey = appKey;
+			UserAccessKey = userAccessKey;
+		}
+	}
+}
