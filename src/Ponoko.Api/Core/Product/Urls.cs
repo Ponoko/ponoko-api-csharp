1b4bc70bb0b2bea098aa8be0e69757a24630d693
Ben Biddington <ben.biddington@gmail.com>
2012-02-11 10:12:05 +1300

    Refactor: Urls data structure doesn't need annotations

--- src/Ponoko.Api/Core/Product/Urls.cs
+++ src/Ponoko.Api/Core/Product/Urls.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using Newtonsoft.Json;
 
 namespace Ponoko.Api.Core.Product {
 	public class Urls {
-		[JsonProperty(PropertyName = "make")]
 		public Uri Make;
-		[JsonProperty(PropertyName = "view")]
 		public Uri View;
 	}
 }
