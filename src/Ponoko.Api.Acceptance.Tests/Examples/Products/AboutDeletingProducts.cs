281a182bef3c34c3dd914418b81d6f1f8a8426ad
Ben Biddington <ben.biddington@gmail.com>
2012-12-04 14:23:05 +1300

    Proof that you cannot delete products with orders

--- src/Ponoko.Api.Acceptance.Tests/Examples/Products/AboutDeletingProducts.cs
+++ src/Ponoko.Api.Acceptance.Tests/Examples/Products/AboutDeletingProducts.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
+using Ponoko.Api.Core.Orders;
+using Ponoko.Api.Core.Orders.Repositories;
+using Ponoko.Api.Core.Product;
 using Ponoko.Api.Core.Product.Commands;
+using Ponoko.Api.Core.Shipping;
+using Ponoko.Api.Core.Shipping.Repositories;
 
 namespace Ponoko.Api.Acceptance.Tests.Examples.Products {
 	[TestFixture]
@@ -14,7 +20,7 @@ namespace Ponoko.Api.Acceptance.Tests.Examples.Products {
 			then_the_product_does_not_exist_with_key(product.Key);
 		}
 		
-		[Test]
+		[Test, Ignore("TEMP: cannot delete orders, or products with orders")]
 		public void you_can_delete_them_all() {
 			var finder = new FindCommand(Internet, Settings.BaseUrl);
 			var delete = new DeleteCommand(Internet, Settings.BaseUrl);
@@ -33,6 +39,88 @@ namespace Ponoko.Api.Acceptance.Tests.Examples.Products {
 
 			Assert.AreEqual(0, finder.FindAll().Length, "Expected them all to have been deleted");
 		}
+
+	    [Test]
+	    public void you_cannot_delete_a_product_that_has_orders()
+	    {
+	        var product = NewProduct("A product for testing deletes");
+	        
+            CreateANewOrder(product);
+	        
+            var theError = Assert.Throws<Exception>(() => new DeleteCommand(Internet, Settings.BaseUrl).Delete(product.Key));
+
+            Assert.That(theError.Message, Is.StringMatching("cannot delete product that has orders"));
+	    }
+
+	    private Order CreateANewOrder(Product product)
+        {
+            var command = new OrderRepository(Internet, Settings.BaseUrl);
+
+            var reference = Guid.NewGuid().ToString();
+
+            var shippingInfo = new ProductShippingInfo() { Key = product.Key, Quantity = 1};
+
+            return command.Create(reference, AnyShippingOption(shippingInfo), ExampleShippingAddress, shippingInfo);
+        }
+
+        private Option AnyShippingOption(ProductShippingInfo shipping)
+        {
+            var allOptions = new ShippingOptionsRepository(Internet, Settings.BaseUrl).
+                For(ExampleAddress, shipping);
+
+            return allOptions.Options.First();
+        }
+
+        protected NameAndAddress ExampleShippingAddress
+        {
+            get
+            {
+                return new NameAndAddress
+                {
+                    FirstName = "Jazz",
+                    LastName = "Kang",
+                    LineOne = "27 Dixon Street",
+                    LineTwo = "Te Aro",
+                    City = "Wellington",
+                    ZipOrPostalCode = "6021",
+                    State = "NA",
+                    Country = "NZ",
+                    Phone = "Any telephone number"
+                };
+            }
+        }
+
+        protected Address ExampleAddress
+        {
+            get
+            {
+                return new Address
+                {
+                    LineOne = "27 Dixon Street",
+                    LineTwo = "Te Aro",
+                    City = "Wellington",
+                    ZipOrPostalCode = "6021",
+                    State = "NA",
+                    Country = "New Zealand"
+                };
+            }
+        }
+
+        protected Address TenDowningStreet
+        {
+            get
+            {
+                return new Address
+                {
+                    LineOne = "10 Downing Street",
+                    LineTwo = "Westminster",
+                    City = "London",
+                    ZipOrPostalCode = "SW1A 2AA",
+                    State = "NA",
+                    Country = "United Kingdom"
+                };
+            }
+        }
 
 		private void then_the_product_does_not_exist_with_key(String id) {
 			var finder = new FindCommand(Internet, Settings.BaseUrl);
