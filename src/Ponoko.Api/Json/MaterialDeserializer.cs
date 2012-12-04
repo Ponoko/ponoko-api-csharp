140cd15d4d35fbe458783b2af42672d27e06c9cf
Ben Biddington <ben.biddington@gmail.com>
2012-12-04 16:09:07 +1300

    Added "dimensions" to MaterialDeserializer's skip list

--- src/Ponoko.Api/Json/MaterialDeserializer.cs
+++ src/Ponoko.Api/Json/MaterialDeserializer.cs
@@ -5,33 +5,46 @@ using Newtonsoft.Json;
 using Ponoko.Api.Core;
 using Ponoko.Api.Sugar;
 
-namespace Ponoko.Api.Json {
-	public static class MaterialDeserializer {
-		public static Material Deserialize(String json) {
-			var settings = new JsonSerializerSettings {
-          		MissingMemberHandling = MissingMemberHandling.Ignore,
-          		Converters = new List<JsonConverter> { new DateTimeReader() }
-			};
+namespace Ponoko.Api.Json
+{
+    public static class MaterialDeserializer
+    {
+        public static Material Deserialize(String json)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                MissingMemberHandling = MissingMemberHandling.Ignore,
+                Converters = new List<JsonConverter> { new DateTimeReader() }
+            };
 
-			var result = JsonConvert.DeserializeObject<Material>(json, settings);
-			result.Attributes.Add(GetExtraAttributes(json));
-			return result;
-		}
+            var result = JsonConvert.DeserializeObject<Material>(json, settings);
+            result.Attributes.Add(GetExtraAttributes(json));
+            return result;
+        }
 
-		private static NameValueCollection GetExtraAttributes(String json) {
+        private static NameValueCollection GetExtraAttributes(String json) {
 			var result = new NameValueCollection();
 
-			var skip = new List<String> {"key", "name", "type", "updated_at"};	
+			var skip = new List<String> {"key", "name", "type", "updated_at", "dimensions"};	
 
 			var material = new Deserializer().Deserialize(json);
 
-			foreach (var attribute in material.Properties()) {
-				un.less(() => skip.Contains(attribute.Name), () => 
-					result.Add(attribute.Name, material.Value<String>(attribute.Name))
-				);
+			foreach (var attribute in material.Properties())
+			{
+			    un.less(() => skip.Contains(attribute.Name), () =>
+			    {
+			        try
+			        {
+			            result.Add(attribute.Name, material.Value<String>(attribute.Name));
+			        }
+			        catch (Exception e)
+			        {
+			            throw new Exception("Failed to get the values for the '" + attribute.Name + "' attribute", e);
+			        }
+			    });
 			}
 
 			return result;
-		} 
-	}
+		}
+    }
 }
